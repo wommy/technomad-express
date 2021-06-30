1a85e3deb50d97f721806e476538d3809560ce3a
wommy <tom@technomad.media>
2021-06-30 18:02:29 -0400

    restarting

--- src/middleware/logger.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction  } from "express";
-
-export default ( req:Request,res:Response,next:NextFunction )=>{
-	console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${new Intl.DateTimeFormat().format()}`);
-	next();
-}
