66b2619727423cd711ac0e57d00e067b393f1b4c
wommy <tom@technomad.media>
2021-07-20 01:19:31 +0000

    feat: redid layout, added todo data

--- src/routes/api/todos.ts
+++ src/routes/api/todos.ts
@@ -1,44 +1,46 @@
-import express from 'express'
-import { v4 as uuidv4 } from 'uuid'
-import todos from '../../database/Todos'
+import express from "express"
+// import { v4 as uuidv4 } from "uuid"
+import todos from "../../database/Todos"
 
 export default express.Router()
-	.get('/', (req,res)=> res.json(todos))
-	.get('/:id', (req,res)=> {
-		todos.some(todo => todo.id === req.params.id)
-		? res.json(todos.filter( todo => todo.id === req.params.id))
-		: res.status(400).json({msg:`no todo with id of ${req.params.id}`})
-	})
-	.post('/', (req,res)=>{
-		const newTodo = {
-			id: uuidv4(),
-			name: req.body.name,
-			isDone: req.body.isDone || false
-		}
-		if(!newTodo.name) {
-			return res.status(400).json({msg:'please include a name'})
-		}
-		todos.push(newTodo)
-		res
-			.json(todos)
-			// .redirect('/')
-	})
-	.put('/:id', (req,res)=> {
-		todos.some(todo => todo.id === req.params.id)
-		? todos.forEach( t => {
-			if(t.id === req.params.id){
-				t.name = req.body.name ? req.body.name : t.name
-				t.isDone = req.body.isDone ? req.body.isDone : t.isDone
-				res.json({msg:'todo updated', t})
-			}
-		})
-		: res.status(400).json({msg:`no todo with id of ${req.params.id}`})
-	})
-	.delete('/:id', (req,res)=> {
-		todos.some(todo => todo.id === req.params.id)
-		? res.json({
-			msg:'todo deleted', 
-			todos: todos.filter( todo => todo.id !== req.params.id)
-		})
-		: res.status(400).json({msg:`no todo with id of ${req.params.id}`})
-	})
+  .get("/", (req, res) => res.json(todos))
+
+  .post("/", (req, res) => {
+    const newTodo = {
+      // id: uuidv4(),
+      id: Date.now(),
+      category: req.body.category || 'inbox',
+      name: req.body.name || '',
+      link: req.body.link,
+    }
+    !newTodo.name && res.status(400).json({ msg: "please include a name" })
+    todos.push(newTodo)
+    // res.json(todos)
+    res.redirect('/')
+  })
+
+  // .get("/:id", (req, res) => {
+  //   todos.some((todo) => todo.id === req.params.id)
+  //     ? res.json(todos.filter((todo) => todo.id === req.params.id))
+  //     : res.status(400).json({ msg: `no todo with id of ${req.params.id}` })
+  // })
+  // .put("/:id", (req, res) => {
+  //   todos.some((todo) => todo.id === req.params.id)
+  //     ? todos.forEach((t) => {
+  //         if (t.id === req.params.id) {
+  //           t.isDone = req.body.isDone ? req.body.isDone : t.isDone
+  //           t.name = req.body.name ? req.body.name : t.name
+  //           t.category = req.body.category ? req.body.category : t.category
+  //           res.json({ msg: "todo updated", t })
+  //         }
+  //       })
+  //     : res.status(400).json({ msg: `no todo with id of ${req.params.id}` })
+  // })
+  // .delete("/:id", (req, res) => {
+  //   todos.some((todo) => todo.id === req.params.id)
+  //     ? res.json({
+  //         msg: "todo deleted",
+  //         todos: todos.filter((todo) => todo.id !== req.params.id),
+  //       })
+  //     : res.status(400).json({ msg: `no todo with id of ${req.params.id}` })
+  // })
