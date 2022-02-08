fbd4e247b0a6370457cb8a9ae4821c7ac09c0d1d
wommy <tom@technomad.media>
2022-02-08 18:53:23 -0500

    YOLO

--- src/database/Todos.ts
+++ src/database/Todos.ts
@@ -4,6 +4,11 @@ export default [
 	{ id: '3', name: 'get todos', isDone: true },
 	{ id: '4', name: 'post todos', isDone: true },
 	{ id: '5', name: 'finish routes', isDone: true },
-	{ id: '6', name: 'templating', isDone: false },
-	{ id: '7', name: 'publish to technomad', isDone: false },
+	{ id: '6', name: 'templating', isDone: true },
+	{ id: '7', name: 'publish to technomad', isDone: true },
+	{ id: '8', name: 'RESUME button download pdf', isDone: false },
+	{ id: '9', name: 'RESUME pdf pandoc', isDone: false },
+	{ id: '10', name: 'RESUME css print', isDone: false },
+	{ id: '11', name: 'RESUME show more', isDone: false },
+	{ id: '12', name: 'RESUME , two sections: main:projects, side:[skills, events]', isDone: false },
 ]
